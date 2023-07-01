ce4b885e71106f5fa8a23f7711e5a88f2584c71b
saltoriousSIG <cryptobet95@gmail.com>
2023-06-30 22:00:41 -0400

    Update fetchRpcUrl.ts

--- src/utils/fetchRpcUrl.ts
+++ src/utils/fetchRpcUrl.ts
@@ -5,12 +5,12 @@ const fetchRpcUrl = () => {
       rpc_url = import.meta.env.VITE_SOLID_APP_DEV_RPC_URL;
       break;
     case "production":
-      rpc_url = import.meta.env.VITE_SOLID_APP_PROD_RPC_URL_2;
+      rpc_url = import.meta.env.VITE_SOLID_APP_PROD_RPC_URL_1;
       break;
   }
   return {
     rpc_url,
-    fallback: import.meta.env.VITE_SOLID_APP_PROD_RPC_URL_1
+    fallback: import.meta.env.VITE_SOLID_APP_PROD_RPC_URL_2
   };
 
 };
